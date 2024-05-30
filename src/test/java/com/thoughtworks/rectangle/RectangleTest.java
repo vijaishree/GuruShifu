f7978ecd35db5a4f11914d053ece0cbab9223e54
Vijaishree Seenivasagan <vijaishreeseenivasagan@gmail.com>
2024-05-30 07:07:20 +0000

    Refactor. test method name for area

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class RectangleTest{
     
     @Test
-    void areaIfLengthIsFourAndBreadthIsFive(){
+    void shouldReturnAreaOfTwentyWhenLengthIsFourAndBreadthIsFive(){
         Rectangle area = new Rectangle();
         assert area.computeArea(4,5) == 20 ;
     }
